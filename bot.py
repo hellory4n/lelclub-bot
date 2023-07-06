35222d94789386e996a7420419a2edc4735186fe
hellory4n <hellory4n@gmail.com>
2023-07-05 20:29:41 -0400

    d

--- bot.py
+++ bot.py
@@ -1,10 +1,17 @@
 import discord
+from discord import app_commands
 import os
 
 client = discord.Client(intents=discord.Intents.default())
+tree = app_commands.CommandTree(client)
 
 @client.event
 async def on_ready():
-    print("sucky")
+    await tree.sync()
+    print("m")
+
+@tree.command(name="ping", description="Check if the bot committed unaliving")
+async def ping(inter):
+    await inter.response.send_message("i'm definitely alive :)")
 
 client.run(os.environ["TOKEN"])

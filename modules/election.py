004c80bba2581e7f4d2f82d906eabcd581234b18
hellory4n <hellory4n@gmail.com>
2023-07-19 01:57:53 -0400

    i can't end elections and i can still get duplicate keys

--- modules/election.py
+++ modules/election.py
@@ -189,6 +189,243 @@ class Election(commands.Cog):
                 await voter.send("Thanks for deciding to vote, this will shape the future of lelclub. Votes are secure and anonymous :)\n\nWho do you want to be the president?", view=view)
             except:
                 await ctx.send(f"{ctx.author.mention} please enable DMs to be able to vote, then try again")
+        else:
+            await ctx.send("The election hasn't started! They do on the 1st of each month tho (unless the bot breaks)")
+
+    @commands.command(name="end-election")
+    async def end_election(self, ctx: commands.Context):
+        """{
+            "started": true,
+            "candidates":
+                {"748560377763201185":
+                    ["President", "Governor of Claps"],
+                "928777777996505148":
+                    ["President", "Governor of Claps", "Governor of Ben State", "Governor of Poop HQ", "Governor of Lelcenter", "Governor of Breat Gritain", "Governor of Builders of La Grasa", "Governor of France 2.0", "Governor of Berkelium", "Governor of The Moon\u2122", "Governor of Finger Island"]},
+            "votes":
+                {"928777777996505148":
+                    ["president:928777777996505148", "The Moon\u2122:928777777996505148"],
+                "748560377763201185":
+                    ["president:748560377763201185", "Ben State:928777777996505148"]}}"""
+        # only allow the founder of lelclub end the election ogmogmgogmogmgogmogkmg
+        if (ctx.author.id == 748560377763201185):
+            await ctx.send("Calculating results...")
+            self.election_stuff["started"] = False
+            self.save_election()
+
+            # make a list of president candidates
+            maybe_presidents = {}
+            for user, roles in self.election_stuff["candidates"].items():
+                if "President" in roles:
+                    maybe_presidents.update({user: 0})
+            print(maybe_presidents)
+
+            # calculate president results
+            for voter, vote in self.election_stuff["votes"].items():
+                # first item in the votes will always be the president, that's how the /vote commands saves it
+                president_choice = vote[0].split(":")[1]
+                maybe_presidents[president_choice] += 1
+            print(maybe_presidents)
+            
+            states = [
+                "Claps",
+                "Lelcenter",
+                "Poop HQ",
+                "Ben State",
+                "Breat Gritain",
+                "Builders of La Grasa",
+                "France 2.0",
+                "Berkelium",
+                "Finger Island",
+                "The Moon™"
+            ]
+
+            # do the same for each state
+            # too lazy to make this less shit it's 1 am
+            # second item in the votes will always be a governor
+            gclaps = {}
+            for user, roles in self.election_stuff["candidates"].items():
+                if "Governor of Claps" in roles:
+                    gclaps.update({user: 0})
+
+            for voter, vote in self.election_stuff["votes"].items():
+                if "Governor of Claps" in vote:
+                    governor_choice = vote[0].split(":")[1]
+                    gclaps[governor_choice] += 1
+            
+            glelcenter = {}
+            for user, roles in self.election_stuff["candidates"].items():
+                if "Governor of Lelcenter" in roles:
+                    glelcenter.update({user: 0})
+
+            for voter, vote in self.election_stuff["votes"].items():
+                if "Governor of Lelcenter" in vote:
+                    governor_choice = vote[0].split(":")[1]
+                    glelcenter[governor_choice] += 1
+            
+            gpoophq = {}
+            for user, roles in self.election_stuff["candidates"].items():
+                if "Governor of Poop HQ" in roles:
+                    gpoophq.update({user: 0})
+
+            for voter, vote in self.election_stuff["votes"].items():
+                if "Governor of Poop HQ" in vote:
+                    governor_choice = vote[0].split(":")[1]
+                    gpoophq[governor_choice] += 1
+            
+            gben = {}
+            for user, roles in self.election_stuff["candidates"].items():
+                if "Governor of Ben State" in roles:
+                    gben.update({user: 0})
+
+            for voter, vote in self.election_stuff["votes"].items():
+                if "Governor of Ben State" in vote:
+                    governor_choice = vote[0].split(":")[1]
+                    gben[governor_choice] += 1
+            
+            gbg = {}
+            for user, roles in self.election_stuff["candidates"].items():
+                if "Governor of Breat Gritain" in roles:
+                    gbg.update({user: 0})
+
+            for voter, vote in self.election_stuff["votes"].items():
+                if "Governor of Breat Gritain" in vote:
+                    governor_choice = vote[0].split(":")[1]
+                    gbg[governor_choice] += 1
+            
+            gbolg = {}
+            for user, roles in self.election_stuff["candidates"].items():
+                if "Governor of Builders of La Grasa" in roles:
+                    gbolg.update({user: 0})
+
+            for voter, vote in self.election_stuff["votes"].items():
+                if "Governor of Builders of La Grasa" in vote:
+                    governor_choice = vote[0].split(":")[1]
+                    gbolg[governor_choice] += 1
+            
+            gfrance = {}
+            for user, roles in self.election_stuff["candidates"].items():
+                if "Governor of France 2.0" in roles:
+                    gfrance.update({user: 0})
+
+            for voter, vote in self.election_stuff["votes"].items():
+                if "Governor of France 2.0" in vote:
+                    governor_choice = vote[0].split(":")[1]
+                    gfrance[governor_choice] += 1
+            
+            gbk = {}
+            for user, roles in self.election_stuff["candidates"].items():
+                if "Governor of Berkelium" in roles:
+                    gbk.update({user: 0})
+
+            for voter, vote in self.election_stuff["votes"].items():
+                if "Governor of Berkelium" in vote:
+                    governor_choice = vote[0].split(":")[1]
+                    gbk[governor_choice] += 1
+            
+            gfinger = {}
+            for user, roles in self.election_stuff["candidates"].items():
+                if "Governor of Finger Island" in roles:
+                    gfinger.update({user: 0})
+
+            for voter, vote in self.election_stuff["votes"].items():
+                if "Governor of Finger Island" in vote:
+                    governor_choice = vote[0].split(":")[1]
+                    gfinger[governor_choice] += 1
+            
+            gmoon = {}
+            for user, roles in self.election_stuff["candidates"].items():
+                if "Governor of The Moon™" in roles:
+                    gmoon.update({user: 0})
+
+            for voter, vote in self.election_stuff["votes"].items():
+                if "Governor of The Moon™" in vote:
+                    governor_choice = vote[0].split(":")[1]
+                    gmoon[governor_choice] += 1
+            
+            # sort stuff :)
+            # the last 100 lines were pure suffering
+            maybe_presidents = sorted(maybe_presidents)
+            gclaps = sorted(gclaps)
+            glelcenter = sorted(glelcenter)
+            gpoophq = sorted(gpoophq)
+            gben = sorted(gben)
+            gbg = sorted(gbg)
+            gbolg = sorted(gbolg)
+            gfrance = sorted(gfrance)
+            gbk = sorted(gbk)
+            gfinger = sorted(gfinger)
+            gmoon = sorted(gmoon)
+
+            # make cool embeds for the results :)
+            president_results = discord.Embed(title="President Results")
+            claps_results = discord.Embed(title="Claps Results")
+            lelcenter_results = discord.Embed(title="Lelcenter Results")
+            poophq_results = discord.Embed(title="Poop HQ Results")
+            ben_results = discord.Embed(title="Ben State Results")
+            breatgritain_results = discord.Embed(title="Breat Gritain Results")
+            bolg_results = discord.Embed(title="Builders of La Grasa Results")
+            france_results = discord.Embed(title="France 2.0 Results")
+            berkelium_results = discord.Embed(title="Berkelium Results")
+            finger_results = discord.Embed(title="Finger Island Results")
+            moon_results = discord.Embed(title="The Moon™ Results")
+
+            # more pain
+            print(maybe_presidents)
+            for ye, mues in maybe_presidents.values:
+                president_results.description += f"<@{ye}>: {mues} votes\n"
+            for ye in gclaps:
+                claps_results.description += f"<@{candidate}>: {votes} votes\n"
+            for ye in glelcenter:
+                lelcenter_results.description += f"<@{candidate}>: {votes} votes\n"
+            for ye in gpoophq:
+                poophq_results.description += f"<@{candidate}>: {votes} votes\n"
+            for ye in gben:
+                ben_results.description += f"<@{candidate}>: {votes} votes\n"
+            for ye in gbg:
+                breatgritain_results.description += f"<@{candidate}>: {votes} votes\n"
+            for ye in gbolg:
+                bolg_results.description += f"<@{candidate}>: {votes} votes\n"
+            for ye in gfrance:
+                france_results.description += f"<@{candidate}>: {votes} votes\n"
+            for ye in gbk:
+                berkelium_results.description += f"<@{candidate}>: {votes} votes\n"
+            for ye in gfinger:
+                finger_results.description += f"<@{candidate}>: {votes} votes\n"
+            for ye in gmoon:
+                moon_results.description += f"<@{candidate}>: {votes} votes\n"
+            
+            # this is really dumb
+            await ctx.send(embeds=[
+                claps_results,
+                lelcenter_results,
+                poophq_results,
+                ben_results,
+                breatgritain_results,
+                bolg_results,
+                france_results,
+                berkelium_results,
+                finger_results,
+                moon_results
+            ])
+            await ctx.send(embed=president_results)
+
+            # one final announcement
+            total_votes = 0
+            for candidate, votes in maybe_presidents.items():
+                total_votes += votes
+            
+            winner_percentage = (total_votes/maybe_presidents[0])*100
+            await ctx.send(f"<@{maybe_presidents[0]}> IS NOW THE PRESIDENT OF THE CAPITALIST UNITED FEDERAL DEMOCRATIC REPUBLIC STATES OF LELCLUB WITH A WHOPPING {winner_percentage}% OF VOTES (we had a total of {total_votes} votes)")
+
+            # reset election data
+            self.election_stuff = {
+                "started": False,
+                "candidates": {},
+                "votes": {}
+            }
+            self.save_election()
+        else:
+            ctx.send("You can't manage the election!")
 
 def setup(client: commands.Bot):
     client.add_cog(Election(client))

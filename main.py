b9c2fd1ee81ba2644f8c99847ec13ba9ae35fd75
hellory4n <hellory4n@gmail.com>
2023-09-22 20:47:05 -0400

    time

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import asyncio
 import json
 import random
 import dotenv
+import datetime
 
 dotenv.load_dotenv(".env")
 
@@ -35,7 +36,36 @@ async def help(ctx):
     cool_embed.add_field(name="Wallets", value="Useful for organizing your money and stuff\n`l.create-wallet name`: create a wallet\n`l.dep amount wallet`: put money in a wallet\n`l.with amount wallet`: remove money from a wallet and put it in cash\n`l.remove-wallet wallet`: removes a wallet", inline=False)
     cool_embed.add_field(name="Items", value="Epic goods and products\n`l.buy item [amount]`: buy an amazing item\n`l.inv [user]`: see the wonderful items you own\n`l.give-item item user [amount]`: give some of your wonderful items to someone else\n`l.new-item`: creates a new item\n`l.edit-item name`: edits an existing item\n`l.remove-item name`: makes an item unavailable for buying\n`l.item-info name`: get info about an item", inline=False)
     cool_embed.add_field(name="Economic policies", value="Useful when you're the CEO of economy\n`l.work-payout min max`: set how much l.work gives\n`l.print-money amount user`: remember that printing money can cause inflation!\n`l.remove-money amount user`: make money magically disappear from existence", inline=False)
+    cool_embed.add_field(name="Other stuff", value="`l.help`: This\n`l.ping`: Check if the bot died\n`l.time`: See the current time across lelclub and allies")
     await ctx.send(embed=cool_embed)
+
+@client.command()
+async def time(ctx):
+    lelisia_standard_time = (datetime.datetime.utcnow() - datetime.timedelta(hours=1)).strftime('%H:%M')
+    lelcity_standard_time = datetime.datetime.utcnow().strftime('%H:%M')
+    france_standard_time = (datetime.datetime.utcnow() - datetime.timedelta(hours=2)).strftime('%H:%M')
+    random_island_standard_time = (datetime.datetime.utcnow() + datetime.timedelta(hours=12)).strftime('%H:%M')
+    brazil_standard_time = (datetime.datetime.utcnow() - datetime.timedelta(hours=3)).strftime('%H:%M')
+    chad_standard_time = (datetime.datetime.utcnow() + datetime.timedelta(hours=1)).strftime('%H:%M')
+    noway_standard_time = (datetime.datetime.utcnow() + datetime.timedelta(hours=2)).strftime('%H:%M')
+    india_standard_time = (datetime.datetime.utcnow() + datetime.timedelta(hours=5.5)).strftime('%H:%M')
+
+    suffering: discord.Embed = discord.Embed(color=discord.Color(0x008cff))
+    suffering.add_field(
+        name="Metropolitan Lelclub",
+        value=f"- :clap: Claps: {lelisia_standard_time}\n- <:lelcube:1025170523744903199> Lelcenter: {lelcity_standard_time}\n- :poop: Poop HQ: {lelcity_standard_time}\n- <:ben:1026507448242143293> Ben State: {lelcity_standard_time}\n- :tea: Breat Gritain: {lelisia_standard_time}\n- :construction_site: Bolg: {france_standard_time}\n- <:thisgivesmepain:1033073361061957751> r/lelcity: {france_standard_time}\n- :french_bread: France 2.0: {france_standard_time}\n- :thumbsup: Finger Island: {france_standard_time}\n- :b: Berkelium: {lelisia_standard_time}",
+        inline=False
+    )
+    suffering.add_field(
+        name="Lelxtremities",
+        value=f"- :new_moon_with_face: The Moon™: {lelisia_standard_time}\n- :penguin: Penguintopia: {lelisia_standard_time}\n- <:ben:1026507448242143293> New Ben: {lelisia_standard_time}\n- :island: Random Islands: {random_island_standard_time}",
+        inline=False
+    )
+    suffering.add_field(
+        name="Other BEANS Members",
+        value=f"- :flag_br: Brazil: {brazil_standard_time} (only on the part that matters, brazil has 4 timezones)\n- :flag_ar: Argentina: {brazil_standard_time}\n- <:noway:1133172484733157396> Noway: {noway_standard_time}\n- <:new_virginia:1133172417871741148> Chad: {chad_standard_time}\n- <:india:1134554468046868610> India: {india_standard_time}"
+    )
+    await ctx.send(embed=suffering)
 
 cool_token = os.getenv("TOKEN")
 version = os.getenv("VERSION")
